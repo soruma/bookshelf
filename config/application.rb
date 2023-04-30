35f9c6f04d9b77ee5299999746486dad22648a5a
soruma <soruma.net@gmail.com>
2023-05-01 00:34:05 +0900

    Change app name

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Myapp
+module Bookshelf
   class Application < Rails::Application
     config.generators do |generate|
       generate.javascripts false
