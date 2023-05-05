93e66d67a3c449ccc60cd48d8b65bb73264bec6e
soruma <soruma.net@gmail.com>
2023-05-05 20:32:57 +0900

    Install eslint and typescript-eslint

--- etc/infrastructure/bookshelfUserPool/lib/bookshelf_user_pool-stack.ts
+++ etc/infrastructure/bookshelfUserPool/lib/bookshelf_user_pool-stack.ts
@@ -1,34 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
-import * as ssm from 'aws-cdk-lib/aws-ssm';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class BookshelfUserPoolStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const domainPrefixParam = new cdk.CfnParameter(this, 'DomainPrefix', {
-      type: 'String',
-      default: 'bookshelf',
+    const domainPrefixParam = new cdk.CfnParameter(this, "DomainPrefix", {
+      type: "String",
+      default: "bookshelf",
     });
 
-    const clientCallbackUrlsParam = new cdk.CfnParameter(this, 'ClientCallbackUrls', {
-      type: 'CommaDelimitedList',
-      default: [
-        'http://localhost:3000/auth/signin',
-        'http://localhost:3000/auth/signup'
-      ].toString(),
-    });
+    const clientCallbackUrlsParam = new cdk.CfnParameter(
+      this,
+      "ClientCallbackUrls",
+      {
+        type: "CommaDelimitedList",
+        default: [
+          "http://localhost:3000/auth/signin",
+          "http://localhost:3000/auth/signup",
+        ].toString(),
+      }
+    );
 
-    const clientLogoutUrlsParam = new cdk.CfnParameter(this, 'ClientLogoutUrls', {
-      type: 'CommaDelimitedList',
-      default: [ 'http://localhost:3000/auth/signout' ].toString(),
-    });
+    const clientLogoutUrlsParam = new cdk.CfnParameter(
+      this,
+      "ClientLogoutUrls",
+      {
+        type: "CommaDelimitedList",
+        default: ["http://localhost:3000/auth/signout"].toString(),
+      }
+    );
 
-    const userPool = new cognito.UserPool(this, 'UserPool', {
-      userPoolName: 'BookshelfUserPool',
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      userPoolName: "BookshelfUserPool",
       accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
-      autoVerify: { email: true, },
+      autoVerify: { email: true },
       deletionProtection: true,
       selfSignUpEnabled: true,
       signInAliases: { username: true, email: true }, // Cognito user pool sign-in options
@@ -38,28 +46,29 @@ export class BookshelfUserPoolStack extends cdk.Stack {
       },
       keepOriginal: { email: true }, // Verifying attribute changes
     });
-    userPool.addDomain('Domain', {
+    userPool.addDomain("Domain", {
       cognitoDomain: { domainPrefix: domainPrefixParam.valueAsString },
     });
 
-    const clientWriteAttributes = (new cognito.ClientAttributes())
-      .withStandardAttributes({fullname: true, email: true});
+    const clientWriteAttributes =
+      new cognito.ClientAttributes().withStandardAttributes({
+        fullname: true,
+        email: true,
+      });
 
-    const clientReadAttributes = clientWriteAttributes
-      .withStandardAttributes({emailVerified: true});
+    const clientReadAttributes = clientWriteAttributes.withStandardAttributes({
+      emailVerified: true,
+    });
 
-    const userPoolClient = userPool.addClient('Client', {
+    const userPoolClient = userPool.addClient("Client", {
       authFlows: {
-        adminUserPassword: true
+        adminUserPassword: true,
       },
       oAuth: {
         flows: {
           authorizationCodeGrant: true,
         },
-        scopes: [
-          cognito.OAuthScope.EMAIL,
-          cognito.OAuthScope.OPENID,
-        ],
+        scopes: [cognito.OAuthScope.EMAIL, cognito.OAuthScope.OPENID],
         callbackUrls: clientCallbackUrlsParam.valueAsList,
         logoutUrls: clientLogoutUrlsParam.valueAsList,
       },
@@ -75,14 +84,18 @@ export class BookshelfUserPoolStack extends cdk.Stack {
       UserPoolClientSecret: userPoolClient.userPoolClientSecret.unsafeUnwrap(),
     };
 
-    const bookshelfUserPoolStringParameter = new ssm.StringParameter(this, 'UserPoolAttributes', {
-      parameterName: 'BookshelfUserPoolAttributes',
-      stringValue: JSON.stringify(bookshelfUserPoolAttributes),
-    });
+    const bookshelfUserPoolStringParameter = new ssm.StringParameter(
+      this,
+      "UserPoolAttributes",
+      {
+        parameterName: "BookshelfUserPoolAttributes",
+        stringValue: JSON.stringify(bookshelfUserPoolAttributes),
+      }
+    );
 
-    new cdk.CfnOutput(this, 'StringParameterArn', {
+    new cdk.CfnOutput(this, "StringParameterArn", {
       value: bookshelfUserPoolStringParameter.parameterArn,
-      exportName: 'BookshelfUserPoolStringParameterArn',
-    })
+      exportName: "BookshelfUserPoolStringParameterArn",
+    });
   }
 }
--- etc/infrastructure/bookshelfUserPool/test/bookshelf_user_pool.test.ts
+++ etc/infrastructure/bookshelfUserPool/test/bookshelf_user_pool.test.ts
@@ -1,38 +1,39 @@
-import * as cdk from 'aws-cdk-lib';
-import { Capture, Template } from 'aws-cdk-lib/assertions';
-import * as BookshelfUserPool from '../lib/bookshelf_user_pool-stack';
+import * as cdk from "aws-cdk-lib";
+import { Capture, Template } from "aws-cdk-lib/assertions";
+import * as BookshelfUserPool from "../lib/bookshelf_user_pool-stack";
 
-test('Cognito UserPoolClient  Created', () => {
+test("Cognito UserPoolClient  Created", () => {
   const app = new cdk.App();
-  const stack = new BookshelfUserPool.BookshelfUserPoolStack(app, 'BookshelfUserPoolTestStack');
+  const stack = new BookshelfUserPool.BookshelfUserPoolStack(
+    app,
+    "BookshelfUserPoolTestStack"
+  );
   const template = Template.fromStack(stack);
 
-  template.hasParameter('DomainPrefix', {
-    Type: 'String'
+  template.hasParameter("DomainPrefix", {
+    Type: "String",
   });
 
-  template.hasParameter('ClientCallbackUrls', {
-    Type: 'CommaDelimitedList'
+  template.hasParameter("ClientCallbackUrls", {
+    Type: "CommaDelimitedList",
   });
 
-  template.hasParameter('ClientLogoutUrls', {
-    Type: 'CommaDelimitedList'
+  template.hasParameter("ClientLogoutUrls", {
+    Type: "CommaDelimitedList",
   });
 
   const allowedOAuthFlows = new Capture();
   template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
     AllowedOAuthFlows: allowedOAuthFlows,
   });
-  expect(allowedOAuthFlows.asArray()).toEqual(
-    expect.arrayContaining(['code'])
-  );
-  
+  expect(allowedOAuthFlows.asArray()).toEqual(expect.arrayContaining(["code"]));
+
   const allowedOAuthScopes = new Capture();
   template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
     AllowedOAuthScopes: allowedOAuthScopes,
   });
   expect(allowedOAuthScopes.asArray()).toEqual(
-    expect.arrayContaining(['email', 'openid'])
+    expect.arrayContaining(["email", "openid"])
   );
 
   const explicitAuthFlows = new Capture();
@@ -40,30 +41,35 @@ test('Cognito UserPoolClient  Created', () => {
     ExplicitAuthFlows: explicitAuthFlows,
   });
   expect(explicitAuthFlows.asArray()).toEqual(
-    expect.arrayContaining(['ALLOW_ADMIN_USER_PASSWORD_AUTH'])
+    expect.arrayContaining(["ALLOW_ADMIN_USER_PASSWORD_AUTH"])
+  );
+
+  const readAttributes = new Capture();
+  template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+    ReadAttributes: readAttributes,
+  });
+  expect(readAttributes.asArray()).toEqual(
+    expect.arrayContaining(["email", "email_verified", "name"])
+  );
+
+  const writeAttributes = new Capture();
+  template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+    WriteAttributes: writeAttributes,
+  });
+  expect(writeAttributes.asArray()).toEqual(
+    expect.arrayContaining(["email", "name"])
   );
 
   template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
     GenerateSecret: true,
   });
+
   template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
-    PreventUserExistenceErrors: 'ENABLED',
+    CallbackURLs: { Ref: "ClientCallbackUrls" },
+    LogoutURLs: { Ref: "ClientLogoutUrls" },
   });
 
-  const callbackUrls = new Capture();
   template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
-    CallbackURLs: callbackUrls,
+    PreventUserExistenceErrors: "ENABLED",
   });
-  expect(explicitAuthFlows.asArray()).toEqual(
-    expect.arrayContaining(['ALLOW_ADMIN_USER_PASSWORD_AUTH'])
-  );
-
-
-  /*
-    callbackUrls: clientCallbackUrlsParam.valueAsList,
-    logoutUrls: clientLogoutUrlsParam.valueAsList,
-  },
-  readAttributes: clientReadAttributes,
-  writeAttributes: clientWriteAttributes,
-  */
 });
