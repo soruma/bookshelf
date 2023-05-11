f93f0537f5873bef421305356852370443ca035b
soruma <soruma.net@gmail.com>
2023-05-12 01:25:43 +0900

    Fix rubocop Security/JSONLoad Cop

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Dir[Rails.root.join('db/seeds/*.json')].each do |path|
   File.open(path) do |file|
     base = File.basename(path, '.json')
     classes = /(\d+)_(.+)/.match(base)[2]
-    objs = JSON.load(file)[classes]
+    objs = JSON.load_file(file)[classes]
 
     klass = classes.singularize.camelize.constantize
     klass.upsert_all(objs, unique_by: :id)
