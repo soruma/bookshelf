2eeb8739cf75fd5ea97e5e35595e81f4a4356761
soruma <soruma.net@gmail.com>
2023-06-07 09:55:54 +0900

    Add is_admin to users

---
+++ db/migrate/20230606161642_add_is_admin_to_users.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddIsAdminToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :is_admin, :boolean, null: false, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_11_163141) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_161642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_11_163141) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email"
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["subscriber"], name: "index_users_on_subscriber", unique: true
